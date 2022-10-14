2ee8b7628a63ba8e4249a3728cae6b5cbc93e5a4
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-14 15:55:20 +0300

    add Telegram contact

--- src/components/GetInTouch/GetInTouch.tsx
+++ src/components/GetInTouch/GetInTouch.tsx
@@ -1,9 +1,27 @@
 import { FC } from 'react';
 
+import TelegramSVG from '@/assets/icons/telegram.svg';
+
 import './GetInTouch.pcss';
 
 export const GetInTouch: FC = (): JSX.Element => (
-  <section>
-    Get In touch
-  </section>
+  <>
+    <p>
+      Get in touch
+    </p>
+    <hr />
+    <ul
+      className='link-list'>
+      <li>
+        <a
+          className='get-in-touch-item'
+          target='_blank'
+          rel='noreferrer'
+          href='https://t.me/showmeartem'>
+          <TelegramSVG />
+          @showmeartem
+        </a>
+      </li>
+    </ul>
+  </>
 );
