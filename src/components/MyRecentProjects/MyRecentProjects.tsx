f0a111b0ccacec9037d06e513dc44d28d3285f42
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 11:59:16 +0300

    add projects

--- src/components/MyRecentProjects/MyRecentProjects.tsx
+++ src/components/MyRecentProjects/MyRecentProjects.tsx
@@ -2,7 +2,10 @@ import { FC } from 'react';
 
 import { ProjectCard } from '../ProjectCard/ProjectCard';
 
+import OnlineStore from '@/assets/images/online-store.png';
+import RsLang from '@/assets/images/rslang.png';
 import Shelter from '@/assets/images/shelter.png';
+import VirtualKeyboard from '@/assets/images/virtual-keyboard.png';
 
 import './MyRecentProjects.pcss';
 
@@ -13,11 +16,29 @@ export const MyRecentProjects: FC = (): JSX.Element => (
       <h3 className="heading-3">
         MY RECENT PROJECTS
       </h3>
-      <ProjectCard
-        imgSource={Shelter}
-        projectName='Pet shelter page'
-        deployLink='https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/shelter/pages/main/index.html'
-      />
+      <div
+        className='flex flex-wrap gap-20'>
+        <ProjectCard
+          imgSource={Shelter}
+          projectName='Pet shelter page'
+          deployLink='https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/shelter/pages/main/index.html'
+        />
+        <ProjectCard
+          imgSource={RsLang}
+          projectName='Interactive English textbook'
+          deployLink='http://rslang-team75.pages.dev/'
+        />
+        <ProjectCard
+          imgSource={OnlineStore}
+          projectName='Online store page'
+          deployLink='https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/online-store/'
+        />
+        <ProjectCard
+          imgSource={VirtualKeyboard}
+          projectName='Virtual keyboard'
+          deployLink='https://a-selyugin.github.io/virtual-keyboard/'
+        />
+      </div>
     </div>
   </section>
 );
