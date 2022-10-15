a6603dd2a19eb191bc14f32ee2044cc9b6a1c98e
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 11:52:11 +0300

    add first project card

--- src/components/MyRecentProjects/MyRecentProjects.tsx
+++ src/components/MyRecentProjects/MyRecentProjects.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import { ProjectCard } from '../ProjectCard/ProjectCard';
 
+import Shelter from '@/assets/images/shelter.png';
+
 import './MyRecentProjects.pcss';
 
 export const MyRecentProjects: FC = (): JSX.Element => (
@@ -11,7 +13,11 @@ export const MyRecentProjects: FC = (): JSX.Element => (
       <h3 className="heading-3">
         MY RECENT PROJECTS
       </h3>
-      <ProjectCard />
+      <ProjectCard
+        imgSource={Shelter}
+        projectName='Pet shelter page'
+        deployLink='https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/shelter/pages/main/index.html'
+      />
     </div>
   </section>
 );
