62b8503feb8e08991aa51be3ce4c172ffc711670
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 11:27:12 +0300

    add ProjectCard component

--- src/components/MyRecentProjects/MyRecentProjects.tsx
+++ src/components/MyRecentProjects/MyRecentProjects.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { ProjectCard } from '../ProjectCard/ProjectCard';
+
 import './MyRecentProjects.pcss';
 
 export const MyRecentProjects: FC = (): JSX.Element => (
@@ -9,7 +11,7 @@ export const MyRecentProjects: FC = (): JSX.Element => (
       <h3 className="heading-3">
         MY RECENT PROJECTS
       </h3>
-
+      <ProjectCard />
     </div>
   </section>
 );
---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+import './ProjectCard.pcss';
+
+export const ProjectCard: FC = (): JSX.Element => (
+  <div
+    className='project-card'>
+    <h3 className="heading-3">
+        THIS IS A CARD
+    </h3>
+  </div>
+);
