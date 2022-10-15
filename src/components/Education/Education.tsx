475a1b262b9e2c57c42bc61a9bbe62d1091244d9
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 13:14:36 +0300

    fix education section

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,12 +1,26 @@
 import { FC } from 'react';
 
+import { EducationItem } from '@/components/EducationItem/';
+
 import './Education.pcss';
 
 export const Education: FC = (): JSX.Element => (
   <section
     className='education'>
     <div className='wrapper-flex'>
-      Education
+      <div className="education__container">
+        <h3 className="heading-3">
+          EDUCATION
+        </h3>
+        <EducationItem
+          heading='ITMO University'
+          info='MA in Automation and Control, 2012 - 2014'
+        />
+        <EducationItem
+          heading='ITMO University'
+          info='BA in Automation and Control, 2008 - 2012'
+        />
+      </div>
     </div>
   </section>
 );
