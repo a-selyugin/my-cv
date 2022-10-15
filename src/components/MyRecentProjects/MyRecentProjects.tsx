f85d72b695246c3b52c7d4994f14ef96cd4b0577
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 11:24:22 +0300

    add MyRecentProjects component

---
+++ src/components/MyRecentProjects/MyRecentProjects.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import './MyRecentProjects.pcss';
+
+export const MyRecentProjects: FC = (): JSX.Element => (
+  <section
+    className='my-recent-projects'>
+    <div className='wrapper'>
+      <h3 className="heading-3">
+        MY RECENT PROJECTS
+      </h3>
+
+    </div>
+  </section>
+);
---
+++ src/components/MyRecentProjects/index.ts
@@ -0,0 +1,1 @@
+export { MyRecentProjects } from './MyRecentProjects';
