c341bbb0e3d3d33d27c530f88ea51a61486158e1
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 11:14:12 +0300

    fix about me style

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -18,10 +18,10 @@ export const AboutMe: FC = (): JSX.Element => (
         <GetInTouch />
       </div>
       <div>
-        <p>Hello, I am Artem, and I’m looking for a job as a front-end developer. </p>
-        <p>Working as a product manager for the last 3 years I discovered my passion to User Interfaces and User Expierience. My urge to learn different UI approaches led me to the front-end development.</p>
-        <p>I also have a wide experience in project management and customer care.</p>
-        <p>Here you can know more about my previous jobs.</p>
+        <p className='main-text'>Hello, I am <span className="text-red-600">Artem</span>, and I’m looking for a job as a <span className="text-red-600">front-end developer</span>. </p>
+        <p className='main-text'>Working as a product manager for the last 3 years I discovered my passion to User Interfaces and User Expierience. My urge to learn different UI approaches led me to the front-end development.</p>
+        <p className='main-text'>I also have a wide experience in project management and customer care.</p>
+        <p className='main-text'><span className="text-red-600">Here</span> you can know more about my previous jobs.</p>
       </div>
     </div>
   </section>
