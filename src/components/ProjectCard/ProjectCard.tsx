da7d0f570d0b82d0f9af078960774b37ede0d4d6
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 12:35:03 +0300

    add icons to overlay

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,23 +1,43 @@
 import './ProjectCard.pcss';
 
+import InfoSVG from '@/assets/icons/about.svg';
+import GitHubSVG from '@/assets/icons/github-white.svg';
+import DeploySVG from '@/assets/icons/web.svg';
+
 interface ProjectCardProps {
   imgSource: string;
   projectName: string;
   deployLink: string;
+  gitHubLink: string;
 }
 
 export const ProjectCard = ({
   imgSource,
   projectName,
   deployLink,
+  gitHubLink,
 }: ProjectCardProps): JSX.Element => (
   <div
     className='project-card'>
-    <img
-      className='project-card__image'
-      src={imgSource}
-      alt={projectName}
-    />
+    <div className='project-card__image'>
+      <img
+        src={imgSource}
+        alt={projectName}
+      />
+      <div className='project-card__overlay'>
+        <div className='links-container'>
+          <a href={deployLink}>
+            <DeploySVG />
+          </a>
+          <a href={gitHubLink}>
+            <GitHubSVG />
+          </a>
+          <button type='button'>
+            <InfoSVG />
+          </button>
+        </div>
+      </div>
+    </div>
     <a
       className='project-card__link'
       href={deployLink}
