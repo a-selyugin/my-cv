4b45ed7bb4de4f5c58026210717351ce0bd7ad63
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-15 11:15:25 +0300

    add MyRecentProjects to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { AboutMe } from './components/AboutMe';
+import { MyRecentProjects } from './components/MyRecentProjects/MyRecentProjects';
 
 import { Header } from '@/components/Header';
 
@@ -6,5 +7,6 @@ export const App = (): JSX.Element => (
   <>
     <Header />
     <AboutMe />
+    <MyRecentProjects />
   </>
 );
