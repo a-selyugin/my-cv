0bb7db22e132c01302508305d81aabda0733e116
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 22:32:41 +0300

    hide desktop menu for mobile view

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,28 +4,29 @@ import './Navbar.pcss';
 
 export const Navbar: FC = (): JSX.Element => (
 
-  <ul
-    className='header__links'>
-    <li>
-      <a href="#experience">
-        Experience
-      </a>
-    </li>
-    <li>
-      <a href="#education">
-        Education
-      </a>
-    </li>
-    <li>
-      <a href="#skills">
-        Skills
-      </a>
-    </li>
-    <li>
-      <a href="#contact">
-        Contact
-      </a>
-
-    </li>
-  </ul>
+  <nav>
+    <ul
+      className='header__links_desktop'>
+      <li>
+        <a href="#experience">
+          Experience
+        </a>
+      </li>
+      <li>
+        <a href="#education">
+          Education
+        </a>
+      </li>
+      <li>
+        <a href="#skills">
+          Skills
+        </a>
+      </li>
+      <li>
+        <a href="#contact">
+          Contact
+        </a>
+      </li>
+    </ul>
+  </nav>
 );
