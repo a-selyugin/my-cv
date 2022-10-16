62503aa454773fc8eea072610bd218153c9c9922
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 15:29:42 +0300

    add OnlineStoreInfo content

--- src/components/ProjectInfo/OnlineStoreInfo/OnlineStoreInfo.tsx
+++ src/components/ProjectInfo/OnlineStoreInfo/OnlineStoreInfo.tsx
@@ -4,6 +4,28 @@ import './OnlineStoreInfo.pcss';
 
 export const OnlineStoreInfo: FC = (): JSX.Element => (
   <div>
-    THIS IS ONLINE STORE INFO
+    <h3
+      className='modal-content__heading'
+    >
+      online store page
+    </h3>
+    <p
+      className='modal-content__text'
+    >
+      The goal was to make online store page with items filtering and sorting.
+       The design was at my discretion.
+    </p>
+    <p
+      className='modal-content__text'
+    >
+      Full task description <a
+        className='main-link'
+        href='https://github.com/rolling-scopes-school/tasks/blob/master/tasks/online-store/README.md'
+        target='_blank'
+        rel='noreferrer'>
+        HERE
+      </a>
+      .
+    </p>
   </div>
 );
