5f49466facfd93dbf8b253eb94ec56b3d38f86a1
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 14:37:53 +0300

    add ShelterInfo

---
+++ src/components/ProjectInfo/ShelterInfo/ShelterInfo.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import './ShelterInfo.pcss';
+
+export const ShelterInfo: FC = (): JSX.Element => (
+  <div>
+    THIS IS SHELTER INFO
+  </div>
+);
---
+++ src/components/ProjectInfo/ShelterInfo/index.ts
@@ -0,0 +1,1 @@
+export { ShelterInfo } from './ShelterInfo';
