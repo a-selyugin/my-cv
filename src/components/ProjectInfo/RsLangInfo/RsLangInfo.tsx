a9b55ce1be4ad2ed1fb47146a1910d0a3c1dcab3
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 14:38:02 +0300

    add RsLangInfo

---
+++ src/components/ProjectInfo/RsLangInfo/RsLangInfo.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import './RsLangInfo.pcss';
+
+export const RsLangInfo: FC = (): JSX.Element => (
+  <div>
+    THIS IS RSLANG INFO
+  </div>
+);
---
+++ src/components/ProjectInfo/RsLangInfo/index.ts
@@ -0,0 +1,1 @@
+export { RsLangInfo } from './RsLangInfo';
