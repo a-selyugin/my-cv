b5d36d433cfe865fbe34100662a0b454e113f671
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 14:16:56 +0300

    replace projects info into object

--- src/components/MyRecentProjects/MyRecentProjects.tsx
+++ src/components/MyRecentProjects/MyRecentProjects.tsx
@@ -12,7 +12,40 @@ import './MyRecentProjects.pcss';
 
 export const MyRecentProjects: FC = (): JSX.Element => {
 
-  const [modalActive, setModalActive] = useState<boolean>(true);
+  const [modalActive, setModalActive] = useState<boolean>(false);
+
+  const moreInfo = (projName: string) => {
+    console.log(projName);
+  };
+
+  const projects = {
+    shelter: {
+      imgSource: Shelter,
+      projectName: 'Pet shelter page',
+      deployLink: 'https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/shelter/pages/main/index.html',
+      githubLink: '/',
+    },
+    rslang: {
+      imgSource: RsLang,
+      projectName: 'Interactive English textbook',
+      deployLink: 'http://rslang-team75.pages.dev/',
+      githubLink: 'https://github.com/RS-Lang-Team75/rslang',
+    },
+    onlineStore: {
+      imgSource: OnlineStore,
+      projectName: 'Online store page',
+      deployLink: 'https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/online-store/',
+      githubLink: '/',
+    },
+    virtualKeyboard: {
+      imgSource: VirtualKeyboard,
+      projectName: 'Virtual keyboard',
+      deployLink: 'https://a-selyugin.github.io/virtual-keyboard/',
+      githubLink: 'https://github.com/a-selyugin/virtual-keyboard/tree/virtual-keyboard',
+    },
+  };
+
+  const { shelter, rslang, onlineStore, virtualKeyboard } = projects;
 
   return (
     <>
@@ -25,32 +58,42 @@ export const MyRecentProjects: FC = (): JSX.Element => {
           <div
             className='projects-container'>
             <ProjectCard
-              imgSource={Shelter}
-              projectName='Pet shelter page'
-              deployLink='https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/shelter/pages/main/index.html'
-              gitHubLink='/' />
+              imgSource={shelter.imgSource}
+              projectName={shelter.projectName}
+              deployLink={shelter.deployLink}
+              githubLink={shelter.githubLink}
+              moreInfoClick={moreInfo}
+            />
             <ProjectCard
-              imgSource={RsLang}
-              projectName='Interactive English textbook'
-              deployLink='http://rslang-team75.pages.dev/'
-              gitHubLink='https://github.com/RS-Lang-Team75/rslang' />
+              imgSource={rslang.imgSource}
+              projectName={rslang.projectName}
+              deployLink={rslang.deployLink}
+              githubLink={rslang.githubLink}
+              moreInfoClick={moreInfo}
+            />
             <ProjectCard
-              imgSource={OnlineStore}
-              projectName='Online store page'
-              deployLink='https://rolling-scopes-school.github.io/a-selyugin-JSFE2022Q1/online-store/'
-              gitHubLink='/' />
+              imgSource={onlineStore.imgSource}
+              projectName={onlineStore.projectName}
+              deployLink={onlineStore.deployLink}
+              githubLink={onlineStore.githubLink}
+              moreInfoClick={moreInfo}
+            />
             <ProjectCard
-              imgSource={VirtualKeyboard}
-              projectName='Virtual keyboard'
-              deployLink='https://a-selyugin.github.io/virtual-keyboard/'
-              gitHubLink='https://github.com/a-selyugin/virtual-keyboard/tree/virtual-keyboard' />
+              imgSource={virtualKeyboard.imgSource}
+              projectName={virtualKeyboard.projectName}
+              deployLink={virtualKeyboard.deployLink}
+              githubLink={virtualKeyboard.githubLink}
+              moreInfoClick={moreInfo}
+            />
           </div>
         </div>
       </section>
       <ModalWindow
         active={modalActive}
         setActive={setModalActive}
-      />
+      >
+      THIS IS CONTENT
+      </ModalWindow>
     </>
   );
 };
