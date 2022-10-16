046d22e4c93c8f580a73ac7b59216ea5736b8d77
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 15:16:16 +0300

    add VirtualKeyboardInfo content

--- src/components/ProjectInfo/VirtualKeyboardInfo/VirtualKeyboardInfo.tsx
+++ src/components/ProjectInfo/VirtualKeyboardInfo/VirtualKeyboardInfo.tsx
@@ -4,6 +4,28 @@ import './VirtualKeyboardInfo.pcss';
 
 export const VirtualKeyboardInfo: FC = (): JSX.Element => (
   <div>
-    THIS IS VirtualKeyboardInfo INFO
+    <h3
+      className='modal-content__heading'
+    >
+      VIRTUAL KEYBOARD
+    </h3>
+    <p
+      className='modal-content__text'
+    >
+      The goal was to make in-browser keyboard which works both from real keyboard
+       and via mouse. The design was at my discretion.
+    </p>
+    <p
+      className='modal-content__text'
+    >
+      Full task description <a
+        className='main-link'
+        href='https://github.com/rolling-scopes-school/tasks/blob/master/tasks/virtual-keyboard/virtual-keyboard-en.md'
+        target='_blank'
+        rel='noreferrer'>
+        HERE
+      </a>
+      .
+    </p>
   </div>
 );
