7ac42d67b55434d866d1e026745713b6a3314d98
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 20:03:47 +0300

    rearrange App,tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { AboutMe } from './components/AboutMe';
-import { Education } from './components/Education';
-import { Footer } from './components/Footer';
-import { MyRecentProjects } from './components/MyRecentProjects';
-import { Skills } from './components/Skills';
+import { MainPage } from './components/MainPage/MainPage';
 
 import { Header } from '@/components/Header';
 
 export const App = (): JSX.Element => (
   <>
     <Header />
-    <main>
-      <AboutMe />
-      <MyRecentProjects />
-      <Education />
-      <Skills />
-      <Footer />
-    </main>
+    <MainPage />
   </>
 );
