5c61390887fcd352ca368a7c7ee2fe3854875886
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-16 15:03:13 +0300

    add more ShelterInfo content

--- src/components/ProjectInfo/ShelterInfo/ShelterInfo.tsx
+++ src/components/ProjectInfo/ShelterInfo/ShelterInfo.tsx
@@ -5,17 +5,22 @@ import './ShelterInfo.pcss';
 export const ShelterInfo: FC = (): JSX.Element => (
   <div>
     <h3
-      className='heading-3'
+      className='modal-content__heading'
     >
       PET SHELTER
     </h3>
-    <p>
+    <p
+      className='modal-content__text'
+    >
       The goal was to make a website according to Figma draft as close as possible. The
        website consist of two pages and includes simple things like sliders and burger menu.
         The trick was to make slider on main page working as infinite shuffle.
     </p>
-    <p>
-      You can check task description <a
+    <p
+      className='modal-content__text'
+    >
+      You can check full task description <a
+        className='main-link'
         href='https://github.com/rolling-scopes-school/tasks/blob/master/stage1/stream1/shelter/README.md'
         target='_blank'
         rel='noreferrer'>
@@ -23,12 +28,16 @@ export const ShelterInfo: FC = (): JSX.Element => (
       </a>
       .
     </p>
-    <p>You can check Figma draft <a
-      href='https://www.figma.com/file/5iCIxvJJbLSk8JBiNlGaFv/shelter-dom-(Copy)'
-      target='_blank'
-      rel='noreferrer'>
+    <p
+      className='modal-content__text'
+    >
+      You can check Figma draft <a
+        className='main-link'
+        href='https://www.figma.com/file/5iCIxvJJbLSk8JBiNlGaFv/shelter-dom-(Copy)'
+        target='_blank'
+        rel='noreferrer'>
         HERE
-    </a>
+      </a>
     .
     </p>
   </div>
