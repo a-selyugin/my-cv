2b50f2685ff54b3f6c764b01be6437693c3298f8
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-17 10:54:31 +0300

    add PreviousJobs component

---
+++ src/components/PreviousJobs/PreviousJobs.tsx
@@ -0,0 +1,11 @@
+import { ReturnModalContentProps } from '@/types/interfaces';
+
+import './PreviousJobs.pcss';
+
+export const PreviousJobs = ({
+  returnModalContent,
+}:ReturnModalContentProps): JSX.Element => (
+  <div>
+    Previous Jobs
+  </div>
+);
