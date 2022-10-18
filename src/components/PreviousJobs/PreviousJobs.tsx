b89495db66aacc0f2844b78101fd38a312819b1a
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-18 13:52:52 +0300

    add PreviousJobs

--- src/components/PreviousJobs/PreviousJobs.tsx
+++ src/components/PreviousJobs/PreviousJobs.tsx
@@ -1,27 +1,24 @@
 import { FC } from 'react';
 
 import './PreviousJobs.pcss';
+import { ProductManager } from './ProductManager/ProductManager';
+import { ProjectManager } from './ProjectManager/ProjectManager';
+import { ServiceEngineer } from './ServiceEngineer/ServiceEngineer';
+import { Technician } from './Technician/Technician';
 
 export const PreviousJobs: FC = (): JSX.Element => (
   <section
     className='wrapper'
   >
     <h3
-      className='modal-content__heading'
+      className='heading-3'
     >
       Previous positions
     </h3>
-    <p
-      className='main-text'
-    >
-      Product Manager - Navis Engineering Oy
-    </p>
-    <p
-      className='modal-content__text'
-    >
-      The goal was to make a website according to Figma draft as close as possible. The
-       website consist of two pages and includes simple things like sliders and burger menu.
-        The trick was to make slider on main page working as infinite shuffle.
-    </p>
+    <ProductManager />
+    <ProjectManager />
+    <ServiceEngineer />
+    <Technician />
+
   </section>
 );
