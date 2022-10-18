16e70df3b1cdce54565cb1eec575c264ae7f3fa4
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-18 14:38:41 +0300

    add ProductManager content

--- src/components/PreviousJobs/ProductManager/ProductManager.tsx
+++ src/components/PreviousJobs/ProductManager/ProductManager.tsx
@@ -32,10 +32,39 @@ export const ProductManager: FC = (): JSX.Element => (
           Creating of new products and features concept
         </li>
         <li>
-          Sales support, meeting participation witch clients on pre-sale stage.
+          Infrastructure websites maintaining
         </li>
         <li>
-          Technical meetings participation on behalf of company.
+          Sales support, meeting participation witch clients on pre-sale stage
+        </li>
+        <li>
+          Technical meetings participation on behalf of company
+        </li>
+      </ul>
+    </div>
+    <div
+      className='jobs__text'
+    >
+      <p className="jobs__underline">Achievements:</p>
+      <ul className='jobs__list'>
+        <li>
+          Delivered a new product (NJoy) including UI/UX development.
+           The new product is based DP system which allows company to
+           cover new markets using existing technologies. Using the same technologies also allows
+           to reduce the cost of production drastically.
+           More info about the NJoy system <a
+            className='main-link'
+            href='https://navisincontrol.com/news/njoy-our-newest-generation-of-joystick-maneuvering-control-system/'
+            target='_blank'
+            rel='noreferrer'>
+          HERE
+          </a>
+        </li>
+        <li>
+          Optimized the SW regression testing which reduced testing time for about 15%
+        </li>
+        <li>
+          Optimized feature lifecycle abandoning some customized and unused features
         </li>
       </ul>
     </div>
