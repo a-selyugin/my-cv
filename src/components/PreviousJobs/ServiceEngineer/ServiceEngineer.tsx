c45476bdb93c3ab1ffa6d86112ac1e1554012e6f
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-18 13:53:27 +0300

    add ServiceEngineer

---
+++ src/components/PreviousJobs/ServiceEngineer/ServiceEngineer.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+
+import './ServiceEngineer.pcss';
+
+export const ServiceEngineer: FC = (): JSX.Element => (
+
+  <>
+    <p
+      className='jobs__heading'
+    >
+    Product Manager - Navis Engineering Oy
+    </p><p
+      className='jobs__subheading'
+    >
+      May 2019 - present
+    </p><p
+      className='jobs__text'
+    >
+      Responsibilities:
+      <ul className='jobs__list'>
+        <li>
+          Software development planning and management
+        </li>
+        <li>
+          Creating of technical specifications
+        </li>
+        <li>
+          Creating of new products and features concept
+        </li>
+        <li>
+          Sales support, meeting participations witch clients on pre-sale stage.
+        </li>
+        <li>
+          Technical meetings participation on behalf of company.
+        </li>
+      </ul>
+    </p>
+  </>
+);
---
+++ src/components/PreviousJobs/Technician/index.ts
@@ -0,0 +1,1 @@
+export { Technician } from './Technician';
