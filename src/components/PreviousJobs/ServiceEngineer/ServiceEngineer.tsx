9eb76065137294c0a3dece5894a639ddbab5ed86
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-10-18 14:02:04 +0300

    add ServiceEngineer content

--- src/components/PreviousJobs/ServiceEngineer/ServiceEngineer.tsx
+++ src/components/PreviousJobs/ServiceEngineer/ServiceEngineer.tsx
@@ -10,30 +10,45 @@ export const ServiceEngineer: FC = (): JSX.Element => (
     <p
       className='jobs__heading'
     >
-    Product Manager - Navis Engineering Oy
+      Field Service Engineer - Navis Engineering Oy
     </p><p
       className='jobs__subheading'
     >
-      May 2019 - present
+      Jan 2014 - Jun 2017
     </p><p
       className='jobs__text'
     >
       Responsibilities:
       <ul className='jobs__list'>
         <li>
-          Software development planning and management
+        Commissioning of Dynamic Positioning Control Systems and Autopilot
+         Systems on board of vessels including minor SW fixes
         </li>
         <li>
-          Creating of technical specifications
+        System presentation to owners and class, meeting participation on behalf of company
         </li>
         <li>
-          Creating of new products and features concept
+        Service cases solving, customer support
         </li>
         <li>
-          Sales support, meeting participations witch clients on pre-sale stage.
+        Providing DP-Maintenance courses and service agents training with further support
         </li>
         <li>
-          Technical meetings participation on behalf of company.
+        Manuals writing and editing
+        </li>
+      </ul>
+    </p>
+    <p
+      className='jobs__text'
+    >
+      Achievements:
+      <ul className='jobs__list'>
+        <li>
+        More than 40 successfully commissioned projects from early 2014 to mid 2017
+        </li>
+        <li>
+          Successfully trained more than 20 ship crew
+           members and 3 service agents from early-to-mid 2017
         </li>
       </ul>
     </p>
